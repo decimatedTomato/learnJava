3d98e8cb9214e66cea70ed2663be3d2ef78b3773
decimatedTomato <greentomatopower@gmail.com>
2024-02-01 02:16:44 +0100

    Added running exercise code explanation

---
+++ exercises/ExerciseRunner.java
@@ -0,0 +1,18 @@
+package exercises;
+
+/**
+ * Maybe you want to use this class to always house the main method
+ * (As we know main methods are annoying)
+ * 
+ * If you want to make use of command line arguments then add another main method inside of the class you are working on
+ * Otherwise simply instantiate that class (we will explain this later)
+ * and maybe call a start method on it
+ * like so:
+ *     new ClassName().start();
+ * (the start method is not magic, you have to write it, I just think thats a name that fits with any program)
+ */
+public class ExerciseRunner {
+    public static void main(String[] args) {
+        // new exercises.E01_HelloWorld.HelloWorld();
+    }
+}
